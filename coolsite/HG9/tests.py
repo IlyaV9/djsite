0db69638befdbe30b497ebfb9d4f4ec325707c07
IlyaV9 <9ailyav9@gmail.com>
2022-05-20 15:55:35 +0500

    Version #5

--- coolsite/HG9/tests.py
+++ coolsite/HG9/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class HG9ApiTestCase(APITestCase):
+    def test_get(self):
+        url = reverse('about')
+        print(url)
+        response = self.client.get(url)
+        print(response)
+
